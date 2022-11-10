1b9180b9ab3d4e89c1865b6733b280ae836fc121
bepyan <65283190+bepyan@users.noreply.github.com>
2022-11-10 16:36:23 +0000

    Deploying to gh-pages from @ bepyan/bepyan.github.io@bf6f088a1ea54a6a1485b1ad5d1a8ac74212b136 🚀

---
+++ _next/static/1pPH3OlReGZFOZ9PLOc_J/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fblog\u002F[...slugs]","\u002Ftags"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/H_Q9-aCu-ImhSBou9BrP5/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Ftags"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
