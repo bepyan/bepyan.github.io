e5478d204af86682ab2caaf52bffc2cbfd76353d
edward <bepyan@naver.com>
2022-11-15 23:16:06 +0900

    chore. next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://bepyan.github.io',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 7000,
+  generateRobotsTxt: true,
+  exclude: [],
+};
