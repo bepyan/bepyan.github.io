b2e6eda67757fb3192ef0247d0e7826d2d924137
edward <bepyan@naver.com>
2022-11-16 13:40:24 +0900

    feat. GA_TRACKING_ID

--- src/libs/gtag.ts
+++ src/libs/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID ?? '';
+export const GA_TRACKING_ID = 'G-27VK5FH7WW';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
