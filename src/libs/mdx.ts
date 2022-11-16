c77ccd31a7158d36d901938aac6e318074c1132a
edward <bepyan@naver.com>
2022-11-16 21:58:42 +0900

    feat. code intelligence styling

--- src/libs/mdx.ts
+++ src/libs/mdx.ts
@@ -1,5 +1,6 @@
 import { serialize } from 'next-mdx-remote/serialize';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeCodeTitles from 'rehype-code-titles';
 import rehypePrism from 'rehype-prism-plus';
 import rehypeSlug from 'rehype-slug';
 import remarkGfm from 'remark-gfm';
@@ -12,6 +13,7 @@ export const serializeMdx = (source: string) => {
       remarkPlugins: [remarkToc, remarkGfm],
       rehypePlugins: [
         rehypeSlug,
+        rehypeCodeTitles,
         rehypePrism,
         [
           rehypeAutolinkHeadings,
