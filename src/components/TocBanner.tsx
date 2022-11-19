ac6245c6279f8bb2a491a16181d6eebc559546c9
edward <bepyan@naver.com>
2022-11-20 01:14:26 +0900

    feat. TOC resize

--- src/components/TocBanner.tsx
+++ src/components/TocBanner.tsx
@@ -46,34 +46,42 @@ const useTableOfContents = (source: string) => {
   useEffect(() => {
     if (tableOfContents.length === 0) return;
 
+    let headings: { id: string; top: number }[];
     const style = window.getComputedStyle(document.documentElement);
     const scrollMt =
       parseFloat(style.getPropertyValue('--scroll-mt').match(/[\d.]+/)?.[0] ?? '0') *
       parseFloat(style.fontSize.match(/[\d.]+/)?.[0] ?? '16');
 
-    const $headings = Array.from(
-      document.querySelectorAll<HTMLElement>(
-        '.prose h2:not(#table-of-contents),h3:not(#table-of-contents)',
-      ),
-    ).map((element) => ({ id: element.id, top: element.offsetTop }));
+    function onResize() {
+      headings = Array.from(
+        document.querySelectorAll<HTMLElement>(
+          '.prose h2:not(#table-of-contents),h3:not(#table-of-contents)',
+        ),
+      ).map((element) => ({ id: element.id, top: element.offsetTop }));
+    }
 
     function onScroll() {
+      if (!headings) return;
+
       const NAV_TOP = 120;
       const top = window.pageYOffset + scrollMt - NAV_TOP + 1;
 
-      let current = $headings[0].id;
-      for (let i = 0; i < $headings.length; i++) {
-        if (top >= $headings[i].top) {
-          current = $headings[i].id;
+      let current = headings[0].id;
+      for (let i = 0; i < headings.length; i++) {
+        if (top >= headings[i].top) {
+          current = headings[i].id;
         }
       }
       setCurrentSectionSlug(current);
     }
 
+    onResize();
     onScroll();
     window.addEventListener('scroll', onScroll, { capture: true, passive: true });
+    window.addEventListener('resize', onResize, { capture: true, passive: true });
     return () => {
       window.removeEventListener('scroll', onScroll, { capture: true });
+      window.removeEventListener('resize', onResize, { capture: true });
     };
   }, [tableOfContents]);
 
