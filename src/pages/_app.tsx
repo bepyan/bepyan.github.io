8428b2fbd06af96fca5dde9f23f53f1d622e34f1
edward <bepyan@naver.com>
2022-12-04 17:11:06 +0900

    feat. google adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -65,6 +65,12 @@ export default function App({ Component, pageProps }: AppProps) {
           `,
           }}
         />
+        {/* google adsense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2820184336962070"
+          crossOrigin="anonymous"
+        />
       </>
     </ThemeProvider>
   );
