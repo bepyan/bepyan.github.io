977b9a5953738b518088fd4d10d7744d1dacd827
edward <bepyan@naver.com>
2022-12-10 02:42:34 +0900

    feat. scroll이 가능할 때만 static header 표시

--- src/pages/archives/tags/[tag].tsx
+++ src/pages/archives/tags/[tag].tsx
@@ -1,5 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import PostListItem from '~/components/common/PostListItem';
 import Title from '~/components/common/Title';
@@ -50,20 +50,34 @@ export default function TagPage({
   const postsRef = useRef<HTMLDivElement>(null);
   const snippetsRef = useRef<HTMLDivElement>(null);
 
+  const [isScrollable, setIsScrollable] = useState(false);
+
+  useEffect(() => {
+    const onResize = () => {
+      setIsScrollable(window.innerHeight < document.body.clientHeight);
+    };
+    onResize();
+
+    window.addEventListener('resize', onResize);
+    return () => {
+      window.removeEventListener('reize', onResize);
+    };
+  }, []);
+
   const scrollToPosts = () => {
     postsRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
   const scrollToSnippets = () => {
-    console.log(snippetsRef.current);
     snippetsRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <Layout>
       <Title className="flex items-end gap-4">
-        Tags - {tag} <span className="text-2xl">({posts.length})</span>
+        Tags - {tag} <span className="text-2xl">({posts.length + snippets.length})</span>
       </Title>
-      {!!posts.length && !!snippets.length && (
+
+      {isScrollable && (
         <div className="sticky top-0 my-4 -mx-2 flex items-center gap-2 bg-gray-100 bg-opacity-80 px-2 py-4 backdrop-blur transition-all dark:bg-gray-900 dark:bg-opacity-50">
           <div
             className="cursor-pointer rounded-lg border border-gray-700 py-1 px-2 font-bold hover:bg-gray-200 dark:border-gray-200 dark:font-normal dark:hover:bg-gray-800"
@@ -79,31 +93,28 @@ export default function TagPage({
           </div>
         </div>
       )}
+
       <div className="mt-8 space-y-12">
-        {!!posts.length && (
-          <div ref={postsRef} className="scroll-m-16">
-            <p className="text-xl font-bold">
-              Posts <span className="text-base">({posts.length})</span>
-            </p>
-            <ul className="mt-2 space-y-4">
-              {posts.map((post) => (
-                <PostListItem key={post.slug} post={post} />
-              ))}
-            </ul>
-          </div>
-        )}
-        {!!snippets.length && (
-          <div ref={snippetsRef}>
-            <p className="text-xl font-bold">
-              Snippets <span className="text-base">({posts.length})</span>
-            </p>
-            <ul className="mt-2 space-y-4">
-              {snippets.map((post) => (
-                <PostListItem key={post.slug} post={post} />
-              ))}
-            </ul>
-          </div>
-        )}
+        <div ref={postsRef} className="scroll-m-16">
+          <p className="text-xl font-bold">
+            Posts <span className="text-base">({posts.length})</span>
+          </p>
+          <ul className="mt-2 space-y-4">
+            {posts.map((post) => (
+              <PostListItem key={post.slug} post={post} />
+            ))}
+          </ul>
+        </div>
+        <div ref={snippetsRef}>
+          <p className="text-xl font-bold">
+            Snippets <span className="text-base">({snippets.length})</span>
+          </p>
+          <ul className="mt-2 space-y-4">
+            {snippets.map((post) => (
+              <PostListItem key={post.slug} post={post} />
+            ))}
+          </ul>
+        </div>
       </div>
     </Layout>
   );
