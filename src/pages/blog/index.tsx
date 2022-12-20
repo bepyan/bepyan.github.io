ddd26a0335c8a7fb6d2760ca6f5717aa46fad83d
edward <bepyan@naver.com>
2022-12-21 00:06:15 +0900

    feat. blog top serize hover 효과 추가

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -62,7 +62,7 @@ export default function PostPage({ serizes, posts }: { serizes: Serize[]; posts:
               style={{ transitionDelay: `${i * 30}ms` }}
             >
               <Link as={serize.slug} href={`/blog/[slug]`}>
-                <div className="relative h-56 w-40 select-none rounded-lg bg-gray-200 px-8 pt-8 pb-12 dark:bg-gray-800">
+                <div className="relative h-56 w-40 select-none rounded-lg bg-gray-200 px-8 pt-8 pb-12 shadow-lg transition-all hover:scale-[1.01] hover:shadow-xl dark:bg-gray-800">
                   <div className="absolute inset-y-0 left-2.5 w-[1px] bg-gray-100 dark:bg-gray-700" />
                   <div className="flex h-full break-keep bg-white px-2 py-3 text-sm font-medium dark:bg-gray-700 dark:text-white">
                     {serize.title}
