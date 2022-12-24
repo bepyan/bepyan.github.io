17ebff20021b009cb44aa9c656de8988a24e074f
edward <bepyan@naver.com>
2022-12-24 23:27:56 +0900

    chore. remove next next-sitemap

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://bepyan.github.io',
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 7000,
-  generateRobotsTxt: true,
-  exclude: [],
-};
