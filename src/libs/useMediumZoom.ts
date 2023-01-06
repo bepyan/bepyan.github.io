836cf14ceb5eedc355bdaf31e6a84983fa695622
edward <bepyan@naver.com>
2023-01-07 00:07:28 +0900

    feat. 테마에 따라 medium zoom 배경 변경

--- src/libs/useMediumZoom.ts
+++ src/libs/useMediumZoom.ts
@@ -1,15 +1,24 @@
-import mediumZoom from 'medium-zoom';
+import mediumZoom, { type Zoom } from 'medium-zoom';
+import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
 export default function useMediumZoom() {
   const [mounted, setMounted] = useState(false);
+  const [zoom, setZoom] = useState<Zoom>();
+
+  const { resolvedTheme } = useTheme();
+  const background = resolvedTheme === 'dark' ? '#131313' : '#f9fafb';
 
   useEffect(() => {
     setMounted(true);
 
     if (mounted) {
       const images = document.querySelectorAll('.prose img');
-      mediumZoom(images, {});
+      setZoom(mediumZoom(images, {}));
     }
   }, [mounted]);
+
+  useEffect(() => {
+    zoom?.update({ background });
+  }, [zoom, background]);
 }
