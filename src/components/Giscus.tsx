055e3a166f586bbac7c274291f16a5dcf850649e
edward <bepyan@naver.com>
2023-01-16 22:55:50 +0900

    feat. dynamic route change with giscus

--- src/components/Giscus.tsx
+++ src/components/Giscus.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from 'next/router';
 import { useTheme } from 'next-themes';
 import { useEffect, useRef } from 'react';
 
 export default function Giscus() {
   const ref = useRef<HTMLDivElement>(null);
   const { resolvedTheme } = useTheme();
+  const router = useRouter();
 
   // https://github.com/giscus/giscus/tree/main/styles/themes
   const theme = resolvedTheme === 'dark' ? 'dark' : 'light';
@@ -36,6 +38,14 @@ export default function Giscus() {
     const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
     iframe?.contentWindow?.postMessage({ giscus: { setConfig: { theme } } }, 'https://giscus.app');
   }, [theme]);
+
+  useEffect(() => {
+    const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
+    iframe?.contentWindow?.postMessage(
+      { giscus: { setConfig: { term: router.asPath } } },
+      'https://giscus.app',
+    );
+  }, [router.asPath]);
 
   return <section ref={ref} />;
 }
